6f85f209eabc3213ae220560206038083fa5a8fa
huangyang <huangyang@xingshulin.com>
2018-01-05 17:13:58 +0800

    测试代码

---
+++ yunlian-upm-admin/src/main/java/com/sinochem/yunlian/upm/api/service/TestService.java
@@ -0,0 +1,9 @@
+package com.sinochem.yunlian.upm.api.service;
+
+/**
+ * @author huangyang
+ * @Description: ${todo}(这里用一句话描述这个类的作用)
+ * @date 2018/01/05 下午5:13
+ */
+public interface TestService {
+}
---
+++ yunlian-upm-admin/src/main/java/com/sinochem/yunlian/upm/api/vo/TestVo.java
@@ -0,0 +1,9 @@
+package com.sinochem.yunlian.upm.api.vo;
+
+/**
+ * @author huangyang
+ * @Description: ${todo}(这里用一句话描述这个类的作用)
+ * @date 2018/01/05 下午5:13
+ */
+public class TestVo {
+}
