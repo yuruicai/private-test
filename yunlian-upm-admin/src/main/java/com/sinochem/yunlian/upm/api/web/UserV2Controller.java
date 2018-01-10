a40037942541ba9ce5b7ab1f092c629a266b4d04
yuruicai <mr_yu17611188161@163.com>
2018-01-10 16:25:19 +0800

    test

--- yunlian-upm-admin/src/main/java/com/sinochem/yunlian/upm/api/web/UserV2Controller.java
+++ yunlian-upm-admin/src/main/java/com/sinochem/yunlian/upm/api/web/UserV2Controller.java
@@ -3,7 +3,10 @@ package com.sinochem.yunlian.upm.api.web;
 import com.sinochem.yunlian.upm.admin.domain.AclUser;
 import com.sinochem.yunlian.upm.api.service.RoleService;
 import com.sinochem.yunlian.upm.api.service.UserService;
+import com.sinochem.yunlian.upm.api.util.ProtectByBase64;
 import com.sinochem.yunlian.upm.api.vo.*;
+import com.sinochem.yunlian.upm.common.security.Base64;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +21,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("inner/api")
+@Slf4j
 public class UserV2Controller {
     @Autowired
     private UserService userService;
@@ -25,6 +29,13 @@ public class UserV2Controller {
     @Autowired
     private RoleService roleService;
 
+    /**
+     * 获取用户列表的方法
+     * @param name
+     * @param page
+     * @param rows
+     * @return
+     */
     @RequestMapping(value = "users/list", method = RequestMethod.GET)
     public Response userList(
             String name,
@@ -33,6 +44,12 @@ public class UserV2Controller {
         PageInfo pageInfo = userService.getUserListByCriteria(name, page, rows);
         return Response.succeed(pageInfo);
     }
+
+    /**
+     * 查询一个用户
+     * @param id
+     * @return
+     */
     @RequestMapping(value = "user/{id}",method = RequestMethod.GET)
     public Response getUser(@PathVariable(value = "id") String id){
         UserByIdVo user = userService.getUserById(id);
@@ -41,6 +58,12 @@ public class UserV2Controller {
         }
         return Response.fail("查询无结果");
     }
+
+    /**
+     * 修改用户信息
+     * @param aclUser
+     * @return
+     */
     @RequestMapping(value = "user/update" , method = RequestMethod.POST)
     public Response updateUser(@RequestBody AclUser aclUser){
         int a = userService.updateUser(aclUser);
@@ -64,6 +87,13 @@ public class UserV2Controller {
         return Response.succeed().put("users", vos);
 
     }
+
+    /**
+     * 电话号码校验
+     * @param id
+     * @param newMobile
+     * @return
+     */
     @RequestMapping(value = "user/mobile/check" , method = RequestMethod.POST)
     public Response userMobileCheck( String id , String newMobile){
         boolean b=userService.mobileCheck(id , newMobile);
@@ -73,5 +103,44 @@ public class UserV2Controller {
         return Response.fail("您输入的号码不符");
     }
 
+    /**
+     * 密码重置
+     * @param id
+     * @param oldPassword
+     * @param newPassword1
+     * @param newPassword2
+     * @return
+     */
+    @RequestMapping(value = "user/password/rest",method = RequestMethod.POST)
+    public Response modPassword(String id ,String oldPassword, String newPassword1, String newPassword2){
+        if(StringUtils.isEmpty(id)){
+            log.info("用户id不能为空！  ， id={}",id);
+            return Response.fail("用户id不能为空。。！");
+        }
+        if(StringUtils.isEmpty(oldPassword)){
+            log.info("用户原密码不能为空！  ， oldPassword={}",oldPassword);
+            return Response.fail("用户原密码不能为空。。！");
+        }
+        if(StringUtils.isEmpty(newPassword1)){
+            log.info("用户新密码不能为空！  ， newPassword1={}",newPassword1);
+            return  Response.fail("用户新密码不能为空。。！");
+        }
+        if(StringUtils.isEmpty(newPassword2)){
+            log.info("用户确认密码不能为空！  ， newPassword2={}",newPassword2);
+            return Response.fail("用户确认密码不能为空。。！");
+        }
+        if( !newPassword1.equals(newPassword2)){
+            log.info("两次新密码不一致！");
+            return   Response.fail("两次新密码不一致。。！");
+        }
+        oldPassword = ProtectByBase64.getBase64(oldPassword);
+        newPassword1 = ProtectByBase64.getBase64(newPassword1);
+        newPassword2 = ProtectByBase64.getBase64(newPassword2);
+        boolean a = userService.restPassword(id,oldPassword,newPassword1,newPassword2);
+        if(!a){
+          return  Response.fail("请您按要求输入。。！");
+        }
+        return Response.succeed();
+    }
 
 }
--- yunlian-upm-admin/src/main/webapp/WEB-INF/views/helper/includeHelper.ftl
+++ yunlian-upm-admin/src/main/webapp/WEB-INF/views/helper/includeHelper.ftl
@@ -1,11 +1,11 @@
-<#assign MenuStatus=statics["com.sinochem.yunlian.upm.admin.constant.MenuStatus"]>
-<#assign ContextAttrType=statics["com.sinochem.yunlian.upm.admin.constant.ContextAttrType"]>
-<#assign MenuType=statics["com.sinochem.yunlian.upm.admin.constant.MenuType"]>
 <#assign CommonStatus=statics["com.sinochem.yunlian.upm.admin.constant.CommonStatus"]>
-<#assign UserRoleSourceType=statics["com.sinochem.yunlian.upm.admin.constant.UserRoleSourceType"]>
-<#assign RoleStatus=statics["com.sinochem.yunlian.upm.admin.constant.RoleStatus"]>
 <#assign RolePermissionRltStatus=statics["com.sinochem.yunlian.upm.admin.constant.RolePermissionRltStatus"]>
-<#assign UserStatus=statics["com.sinochem.yunlian.upm.admin.constant.UserStatus"]>
+<#assign MenuStatus=statics["com.sinochem.yunlian.upm.admin.constant.MenuStatus"]>
+<#assign RoleStatus=statics["com.sinochem.yunlian.upm.admin.constant.RoleStatus"]>
+<#assign UserRoleSourceType=statics["com.sinochem.yunlian.upm.admin.constant.UserRoleSourceType"]>
+<#assign ContextAttrType=statics["com.sinochem.yunlian.upm.admin.constant.ContextAttrType"]>
 <#assign UserRoleType=statics["com.sinochem.yunlian.upm.admin.constant.UserRoleType"]>
+<#assign MenuType=statics["com.sinochem.yunlian.upm.admin.constant.MenuType"]>
 <#assign UserType=statics["com.sinochem.yunlian.upm.admin.constant.UserType"]>
+<#assign UserStatus=statics["com.sinochem.yunlian.upm.admin.constant.UserStatus"]>
 <#assign SMSStatus=statics["com.sinochem.yunlian.upm.admin.constant.SMSStatus"]>
